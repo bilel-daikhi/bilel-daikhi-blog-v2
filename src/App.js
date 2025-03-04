85231218541f9b230ff53b03d740ddc44d43cfd9
eminnance <eminnance@gmail.com>
2025-03-04 13:21:06 +0100

    edit github actions workflow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./lib/routes";
-
+// new comment to publish on git hub
 function App() {
   return (
     <>
